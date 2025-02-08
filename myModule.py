58d8bade4f2ff30013c082eb0502ae324224b956
Shaikh661 <meusmanshahzad@gmail.com>
2025-02-08 16:12:57 +0500

    Add files via upload

---
+++ myModule.py
@@ -0,0 +1,100 @@
+# !>>--> Built-in Modules <--<< 
+import time
+import datetime
+import shutil
+import os
+import re
+import asyncio
+import threading
+import concurrent.futures
+import multiprocessing
+import turtle
+import colorsys
+import math
+import tkinter
+import keyword
+import random
+import secrets
+import enum
+import inspect
+# !>>--> External Modules <--<< 
+import plyer 
+import pyjokes # python jokes
+import pyttsx3 # python text to speech
+import numpy
+import matplotlib # have to be checkedout
+import matplotlib.colors
+import contourpy # have to be checkedout
+import six # have to be checkedout
+import pyparsing # have to be checkedout
+import packaging # have to be checkedout
+import kiwisolver # have to be checkedout
+import cycler # have to be checkedout
+import qrcode
+# import pillow
+
+# import apikey
+# import pygame # python games
+# import itemgetter
+# import fonttools
+# import python-dateutil
+
+
+
+
+
+'''
+#   ~ 
+# ->  There from you can go on to code,
+#       The above area is for Moddules to be imported.
+# ->  The singel line commented '#' modules have to be resurched,
+#       that what is the problem with them.
+#   ~ 
+'''
+# Don't use 'pass' in your code.
+# It make no error when you call this func().
+# def usman():
+#   pass
+
+# # Use 'raise NotImplementedError' in your code.
+# # It makes 'NotImplementedError' error when you call this func().
+# def shaikh():
+#   raise NotImplementedError('''
+# =>\033[33mIt seems that you \033[35mhaven't define \033[33mthe \033[36mfunc() \033[33myet.
+# -> \033[35mGo \033[33mand \033[35mDefine \033[33mthe \033[36mfunc() \033[33mfirst\033[33m.
+# In order to make it \033[35mwork\033[33m.\033[0m''')
+
+
+# def my_pass() -> NotImplementedError:
+#   raise NotImplementedError('''
+# =>\033[33mIt seems that you \033[35mhaven't define \033[33mthe \033[36mfunc() \033[33myet.
+# -> \033[35mGo \033[33mand \033[35mDefine \033[33mthe \033[36mfunc() \033[33mfirst\033[33m.
+# In order to make it \033[35mwork\033[33m.\033[0m''')
+
+
+# !>>--> My Functions <--<< 
+def start() -> None:
+  ''' ~ starting signature code function ~ '''
+  print(f"\n>>> \033[33m ~ $@!kh ~\033[0m\n")
+  # return
+
+def my_pass(*args, **kwargs):
+    caller_name = inspect.stack()[1].function
+    raise NotImplementedError(f'This function has not been implemented yet. Called from "{caller_name}()".')
+
+def exit() -> None:
+  ''' ~ ending signature code function ~ '''
+  print(f"\n>>> \033[33m ~ $@!kh ~\033[0m")
+  time.sleep(0.5)
+  print(f">>> \033[36mcode.exit()\033[0m\n")
+  # return
+
+
+if __name__ == "__main__":
+  # usman()
+  # shaikh()
+  my_pass()
+  start()
+  exit()
+# Installing collected packages: six, pyparsing, pillow, packaging, numpy, kiwisolver, fonttools, cycler, python-dateutil, contourpy, matplotlib, matplotlib.colors
+# Find list of more meta characters here - https://www.ibm.com/docs/en/rational-clearquest/9.0.1?topic=tags-meta-characters-in-regular-expressions
